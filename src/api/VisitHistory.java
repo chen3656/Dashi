d34317d0b0c8e7848a006f5e3e8a72d187331a62
Your Name <chen3656@umn.edu>
2017-03-06 22:23:41 -0600

    First commit

---
+++ src/api/VisitHistory.java
@@ -0,0 +1,133 @@
+package api;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.MongoDBConnection;
+import db.MySQLDBConnection;
+
+/**
+ * Servlet implementation class SearchHistory
+ */
+@WebServlet("/history")
+public class VisitHistory extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final DBConnection connection = new MySQLDBConnection();
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public VisitHistory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// DBConnection connection = new MySQLDBConnection();
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.setStatus(403);
+			return;
+		}
+		try {
+
+			if (request.getParameterMap().containsKey("user_id")) {
+				// term is null or empty by default
+				//String userId = request.getParameter("user_id");
+				String userId = (String) session.getAttribute("user");
+				Set<String> visited_business_id = connection.getVisitedRestaurants(userId);
+				JSONArray array = new JSONArray();
+				for (String id : visited_business_id) {
+					array.put(connection.getRestaurantsById(id, true));
+				}
+				RpcParser.writeOutput(response, array);
+			} else {
+				RpcParser.writeOutput(response, new JSONObject().put("status", "InvalidParameter"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// allow access only if session exists
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.setStatus(403);
+			return;
+		}
+		try {
+
+			JSONObject input = RpcParser.parseInput(request);
+			if (input.has("user_id") && input.has("visited")) {
+				String userId = (String) input.get("user_id");
+				JSONArray array = (JSONArray) input.get("visited");
+				List<String> visitedRestaurants = new ArrayList<>();
+				for (int i = 0; i < array.length(); i++) {
+					String businessId = (String) array.get(i);
+					visitedRestaurants.add(businessId);
+				}
+				connection.setVisitedRestaurants(userId, visitedRestaurants);
+				RpcParser.writeOutput(response, new JSONObject().put("status", "OK"));
+			} else {
+				RpcParser.writeOutput(response, new JSONObject().put("status", "InvalidParameter"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doDelele(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// allow access only if session exists
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.setStatus(403);
+			return;
+		}
+		try {
+			JSONObject input = RpcParser.parseInput(request);
+			if (input.has("user_id") && input.has("visited")) {
+				String userId = (String) input.get("user_id");
+				JSONArray array = (JSONArray) input.get("visited");
+				List<String> visitedRestaurants = new ArrayList<>();
+				for (int i = 0; i < array.length(); i++) {
+					String businessId = (String) array.get(i);
+					visitedRestaurants.add(businessId);
+				}
+				connection.unsetVisitedRestaurants(userId, visitedRestaurants);
+				RpcParser.writeOutput(response, new JSONObject().put("status", "OK"));
+			} else {
+				RpcParser.writeOutput(response, new JSONObject().put("status", "InvalidParameter"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/api/fun.java
@@ -0,0 +1,83 @@
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class fun
+ */
+@WebServlet("/fun")
+public class fun extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public fun() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+
+		String username = "";
+		if (request.getParameter("username") != null) {
+			username = request.getParameter("username");
+		}
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("username", username);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		PrintWriter out = response.getWriter();
+		out.print(obj);
+		out.flush();
+		out.close();
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			JSONObject input = RpcParser.parseInput(request);
+			if (input.has("user_id") && input.has("visited")) {
+				String userId = (String) input.get("user_id");
+				JSONArray array = (JSONArray) input.get("visited");
+				List<String> visitedRestaurants = new ArrayList<>();
+				for (int i = 0; i < array.length(); i++) {
+					String businessId = (String) array.get(i);
+					visitedRestaurants.add(businessId);
+				}
+				RpcParser.writeOutput(response,
+						new JSONObject().put("status", "OK"));
+			} else {
+				RpcParser.writeOutput(response,
+						new JSONObject().put("status", "InvalidParameter"));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
